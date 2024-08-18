90f44780c88737f4d22bd2c6b89f09c8ae3b686e
Chavy <chavy7032@gmail.com>
2024-08-18 20:40:11 +0300

    add

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { RegistrationService } from './register.service';
+import { Subscription } from 'rxjs/internal/Subscription';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
+  
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent  {
+  subscription = new Subscription();
+  readonly registerService = inject(RegistrationService);
+  registrationForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private registrationService: RegistrationService) {
+    this.registrationForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      phone: ['', Validators.required]
+    });
+  }
+  /*registrationForm = new FormBuilder();
+  constructor(private fb: FormBuilder, private registrationService: RegistrationService) {
+  }
+  ngOnInit(): void {
+    this.buildForm();
+  }
+  buildForm(){
+    this.registrationForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      phone: ['', Validators.required]
+    });
+  }*/
+  // פונקציה להירשם שתבוצע בלחיצה על הכפתור
+  register() {
+    console.log('Customer registered:', this.registrationForm);
+    this.subscription.add(this.registerService.register(this.registrationForm).subscribe(response => {
+      console.log('Registration successful', response);
+    }, error => {
+      console.error('Registration failed', error);
+    })
+  )
+  }
 
 }

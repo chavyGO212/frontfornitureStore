245c06cd49fdd70445ceb415406640f26dd80bfc
Chavy <chavy7032@gmail.com>
2024-08-20 12:20:32 +0300

    register and personal log in

--- src/app/personal-log-in/personal-log-in.component.ts
+++ src/app/personal-log-in/personal-log-in.component.ts
@@ -1,18 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RegistrationService } from '../register/register.service';
+
+
+
 
 @Component({
   selector: 'app-personal-log-in',
   templateUrl: './personal-log-in.component.html',
   styleUrls: ['./personal-log-in.component.css']
 })
-export class PersonalLogInComponent {
-  user={
-    firstName:'',
-    lastName:'',
-    phone: '',
-    email:'',
-    //need to add payments
- //need to add orders
-   
+export class PersonalLogInComponent implements OnInit  {
+  customer: any;
+
+  constructor(private registrationService: RegistrationService, private route: ActivatedRoute) { }
+
+
+  ngOnInit(): void {
+    const customerEmail = this.route.snapshot.paramMap.get('email'); 
+    if (customerEmail !== null) {
+      this.registrationService.getCustomerDetailsByEmail(customerEmail).subscribe(data => {
+        this.customer = data;
+      });
+    } else {
+      console.error('Customer email is null');
+      // כאן תוכל להוסיף טיפול במקרה שבו customerEmail הוא null
+    }
   }
+  
 }
+
+
+
+
+  
+
+
+
